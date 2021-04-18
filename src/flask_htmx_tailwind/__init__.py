762c3df06528d3ca3dec9750754ea69d27ca939f
Gary Jarrel <gary@jarrel.com.au>
2021-04-18 12:43:56 +1000

    Adds the htmx section of the article

--- src/flask_htmx_tailwind/__init__.py
+++ src/flask_htmx_tailwind/__init__.py
@@ -9,11 +9,15 @@ def create_app():
 
     assets = Environment(app)
     css = Bundle("main.css", output="dist/main.css", filters="postcss")
+    js = Bundle("js/*.js", output="dist/main.js")
 
     assets.register("css", css)
+    assets.register("js", js)
     css.build()
+    js.build()
 
     from .main import main as main_blueprint
+
     app.register_blueprint(main_blueprint)
 
     return app
--- src/flask_htmx_tailwind/main.py
+++ src/flask_htmx_tailwind/main.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
+from .todo import todos
 
 main = Blueprint('main', __name__)
 
@@ -9,3 +11,17 @@ def homepage():
 @main.route("/google")
 def google():
     return render_template('google.html')
+
+@main.route('/search', methods=['POST'])
+def search():
+    search_term = request.form.get('search')
+
+    if not search_term:
+        return render_template('todo.html', todos=[])
+
+    res_todos = []
+    for todo in todos:
+        if search_term in todo['title']:
+            res_todos.append(todo)
+
+    return render_template('todo.html', todos=res_todos)
