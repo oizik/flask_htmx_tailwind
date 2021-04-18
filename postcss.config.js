59911876d91e2896b344dda63e93d5b488ad065e
Gary Jarrel <gary@jarrel.com.au>
2021-04-18 13:07:00 +1000

    Adjusts to use the FLASK_ENV + corrects the path

--- postcss.config.js
+++ postcss.config.js
@@ -6,9 +6,11 @@ module.exports = (ctx) => ({
   plugins: [
     require("tailwindcss")(path.resolve(__dirname, "tailwind.config.js")),
     require("autoprefixer"),
-    process.env.FLASK_PROD === "production" &&
+    //changed to use FlASK_ENV
+    process.env.FLASK_ENV === "production" &&
       require("@fullhuman/postcss-purgecss")({
-        content: [path.resolve(__dirname, "templates/**/*.html")],
+        // a note on paths: https://jakubsvehla.me/posts/setting-up-tailwind-css-with-flask/
+        content: [path.resolve(__dirname, "src/flask_htmx_tailwind/**/*.html")],
         defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
       }),
   ],
